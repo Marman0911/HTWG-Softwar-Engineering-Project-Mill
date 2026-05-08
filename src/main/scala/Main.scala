f5418b5c62f3e4fa50b81d6e6faaf030916b462e
Marin Corluka <ma211cor@htwg-konstanz.de>
2026-05-08 09:03:11 +0200

    Main zum testen geändert

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,32 @@
-@main def millBC(): Unit =
+/*@main def millBC(): Unit =
   for n <- 1 to 3 do
     val board = MillBoard(n)
     println(s"Board Size")
     println(board.render)
     println()
+    */
+    import scala.io.StdIn.readLine
+
+@main def millGame(): Unit =
+
+  val board = MillBoard()
+
+  val player1 = Player(PlayerId.One)
+  val player2 = Player(PlayerId.Two)
+
+  println("Welcome to Nine Men's Morris!")
+  println()
+
+  println(board.render)
+  println()
+
+  println(s"Player 1 stones in hand: ${player1.stonesInHand}")
+  println(s"Player 2 stones in hand: ${player2.stonesInHand}")
+  println()
+
+  print("Player 1 enter a move: ")
+
+  val input = readLine()
+
+  println()
+  println(s"You entered: $input")
